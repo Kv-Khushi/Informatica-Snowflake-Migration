a95702548ec5a138c7c383954be395781fe79406
Gautam Andani <gautam.andani@nucleusteq.com>
2025-05-12 01:45:17 +0530

    adding folder like how it's added in og repo

---
+++ informatica_replica/Sample1 (2)_2293/databricks_workflow/Env.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# COMMAND ----------
+database = "DELTA_TRAINING"
+cluster_id = ""
+instance_id = ""
+secodary_contact_user_name = ""
+primary_contact_user_name = ""
+api_version = "/api/2.1/"
+auth_token = ""
